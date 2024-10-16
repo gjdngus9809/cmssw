e474c91ded0ceadf874be91b8b9e410f8396f4ba
Woohyeon Heo <gjdngus9809@gmail.com>
2024-10-16 18:25:23 +0900

    add ostream orerride to ME0Stub

--- DataFormats/GEMRecHit/interface/ME0Stub.h
+++ DataFormats/GEMRecHit/interface/ME0Stub.h
@@ -4,16 +4,22 @@
 #include <vector>
 #include <cstdint>
 #include <string>
+#include <iostream>
 
 class ME0Stub {
 private:
     bool ignore_bend = false;
     int lc, hc, id, strip, partition;
-    std::vector<float> centroid;
-    float substrip = 0.0;
-    float bend_ang = 0.0;
+    double bx = -9999;
+    std::vector<double> centroid;
+    double substrip = 0.0;
+    double bend_ang = 0.0;
+    double mse = 9999;
     int quality = 0;
+    int max_cluster_size=0;
+    int max_noise=0;
 public:
+    // Constructors
     ME0Stub();
     ME0Stub(int lc_,
             int hc_,
@@ -25,36 +31,66 @@ public:
             int id_,
             int strip_,
             int partition_,
-            std::vector<float>& centroid_);
+            double bx_);
+    ME0Stub(int lc_,
+            int hc_,
+            int id_,
+            int strip_,
+            int partition_,
+            double bx_,
+            std::vector<double>& centroid_);
+
+    // clone
     ME0Stub* clone() const { return new ME0Stub(*this); }
+
+    // Get private variable
     int LayerCount() const {return lc;}
     int HitCount() const {return hc;}
     int PatternId() const {return id;}
     int Strip() const {return strip;}
     int EtaPartition() const {return partition;}
-    float SubStrip() const {return substrip;}
-    float BendingAngle() const {return bend_ang;}
-    std::vector<float> Centroids() const {return centroid;}
+    int BX() const {return bx;}
+    double SubStrip() const {return substrip;}
+    double BendingAngle() const {return bend_ang;}
+    double MSE() const {return mse;}
+    std::vector<double> Centroids() const {return centroid;}
     int Quality() const {return quality;}
+    int MaxClusterSize() const {return max_cluster_size;}
+    int MaxNoise() const {return max_noise;}
 
+    // Set private variable
     void SetLayerCount(int lc_) {lc=lc_;}
     void SetHitCount(int hc_) {hc=hc_;}
     void SetPatternId(int id_) {id=id_;}
     void SetStrip(int strip_) {strip=strip_;}
     void SetEtaPartition(int partition_) {partition=partition_;}
-    void SetCentroids(std::vector<float> centroid_) {centroid=centroid_;}
+    void SetBX(double bx_) {bx=bx_;}
+    void SetCentroids(std::vector<double> centroid_) {centroid=centroid_;}
+    void SetMaxClusterSize(int max_cluster_size_) {max_cluster_size=max_cluster_size_;}
+    void SetMaxNoise(int max_noise_) {max_noise=max_noise_;}
 
     void reset();
     void update_quality();
     void fit(int max_span=37);
-    // std::string to_string() const;
-    bool operator==(const ME0Stub& other);
-    bool operator>(const ME0Stub& other);
-    bool operator<(const ME0Stub& other);
-    bool operator>=(const ME0Stub& other);
-    bool operator<=(const ME0Stub& other);
+
+    // operators
+    bool operator==(const ME0Stub& other) {
+        if (lc == 0 && other.lc == 0) {return true;}
+        return (quality == other.quality);
+    }
+    bool operator>(const ME0Stub& other) {return (quality > other.quality);}
+    bool operator<(const ME0Stub& other) {return (quality < other.quality);}
+    bool operator>=(const ME0Stub& other) {return (quality >= other.quality);}
+    bool operator<=(const ME0Stub& other) {return (quality <= other.quality);}
+    // ostream
+    friend std::ostream& operator<<(std::ostream& os, const ME0Stub& stub) {
+        os << "id=" << stub.PatternId() << ", lc=" << stub.LayerCount() 
+           << ", strip=" << stub.Strip() << ", prt=" << stub.EtaPartition()
+           << ", quality=" << stub.Quality();
+        return os;
+    }
 private:
-    std::vector<float> llse_fit(const std::vector<float>& x, const std::vector<float>& y);
+    std::vector<double> llse_fit(const std::vector<double>& x, const std::vector<double>& y);
 };
 
 #endif
--- DataFormats/GEMRecHit/src/ME0Stub.cc
+++ DataFormats/GEMRecHit/src/ME0Stub.cc
@@ -17,8 +17,18 @@ ME0Stub::ME0Stub(int lc_,
                  int id_,
                  int strip_,
                  int partition_,
-                 std::vector<float>& centroid_) :
-                lc{lc_}, hc{hc_}, id{id_}, strip{strip_}, partition{partition_}, centroid{centroid_} {
+                 double bx_) :
+                lc{lc_}, hc{hc_}, id{id_}, strip{strip_}, partition{partition_}, bx{bx_} {
+    update_quality();
+}
+ME0Stub::ME0Stub(int lc_,
+                 int hc_,
+                 int id_,
+                 int strip_,
+                 int partition_,
+                 double bx_,
+                 std::vector<double>& centroid_) :
+                lc{lc_}, hc{hc_}, id{id_}, strip{strip_}, partition{partition_}, bx{bx_}, centroid{centroid_} {
     update_quality();
 }
 void ME0Stub::reset() {
@@ -35,56 +45,45 @@ void ME0Stub::update_quality() {
 }
 void ME0Stub::fit(int max_span) {
     if (id!=0) {
-        std::vector<float> tmp;
-        for (float cent : centroid) {
+        std::vector<double> tmp;
+        for (double cent : centroid) {
             tmp.push_back(cent-(max_span/2+1));
         }
-        std::vector<float> x;
-        std::vector<float> centroids;
+        std::vector<double> x;
+        std::vector<double> centroids;
         for (uint32_t i=0; i < tmp.size(); ++i) {
             if (tmp[i] != -1*(max_span/2+1)) {
                 x.push_back(i-2.5);
                 centroids.push_back(tmp[i]);
             }
         }
-        std::vector<float> fit = llse_fit(x, centroids);
+        std::vector<double> fit = llse_fit(x, centroids);
         bend_ang = fit[0];
         substrip = fit[1];
+        mse      = fit[2];
     }
 }
-std::vector<float> ME0Stub::llse_fit(const std::vector<float>& x, const std::vector<float>& y) {
-    float x_sum = 0;
-    float y_sum = 0;
-    for (float val : x) {x_sum += val;}
-    for (float val : y) {y_sum += val;}
+std::vector<double> ME0Stub::llse_fit(const std::vector<double>& x, const std::vector<double>& y) {
+    double x_sum = 0;
+    double y_sum = 0;
+    for (double val : x) {x_sum += val;}
+    for (double val : y) {y_sum += val;}
     int n = x.size();
     // linear regression
-    float product = 0;
-    float squares = 0;
+    double product = 0;
+    double squares = 0;
     for (int i=0; i<n; ++i) {
         product += (n*x[i] - x_sum)*(n*y[i] - y_sum);
         squares += (n*x[i] - x_sum)*(n*x[i] - x_sum);
     }
 
-    float m = product/squares;
-    float b = (y_sum - m*x_sum)/n;
-    std::vector<float> fit = {m, b};
+    double m = product/squares;
+    double b = (y_sum - m*x_sum)/n;
+    double sse = 0.0;
+    for (int i=0; i<n; ++i) {
+        sse += (y[i] - m*x[i] - b)*(y[i] - m*x[i] - b);
+    }
+
+    std::vector<double> fit = {m, b, sse/n};
     return fit;
-}
-// std::string ME0Stub::to_string() const {}
-bool ME0Stub::operator==(const ME0Stub& other) {
-    if (lc == 0 && other.lc == 0) {return true;}
-    return (quality == other.quality);
-}
-bool ME0Stub::operator>(const ME0Stub& other) {
-    return (quality > other.quality);
-}
-bool ME0Stub::operator<(const ME0Stub& other) {
-    return (quality < other.quality);
-}
-bool ME0Stub::operator>=(const ME0Stub& other) {
-    return (quality >= other.quality);
-}
-bool ME0Stub::operator<=(const ME0Stub& other) {
-    return (quality <= other.quality);
 }
